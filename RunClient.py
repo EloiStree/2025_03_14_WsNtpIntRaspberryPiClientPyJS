231449a2e18fdb21dc2b3d8f92c854cc9d3de65a
Eloi Stree <eloistree@gmail.com>
2025-03-14 16:59:10 +0100

    Save

---
+++ RunClient.py
@@ -0,0 +1,20 @@
+print("Hello World")
+print("Hello World2")
+
+import web3
+import os
+relative_file_path_private_eth_key ="Keys/private.txt"
+relative_file_path_public_eth_key ="Keys/public.txt"
+
+ntp_server = "apint.ddns.net"
+ws_int_server = "ws://apint.ddns.net/4615"
+
+
+# bool_is_private_key_file_exist= os.path.exists(relative_file_path_private_eth_key)
+# if not bool_is_private_key_file_exist:
+    
+    
+
+
+
+
